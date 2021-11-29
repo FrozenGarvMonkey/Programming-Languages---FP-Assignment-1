bb895700e626436942ba37ce461ab905b55fb85a
Garv Nair <32934519+garvnair@users.noreply.github.com>
2021-11-29 09:01:41 +0800

    Added find_expenses and changed mock data

--- main.py
+++ main.py
@@ -1,41 +1,56 @@
 from functools import reduce
 from datetime import datetime
 import json
+
 ############################################################################################
 
 # Case Study Link: https://uxplanet.org/expenses-management-app-a-ux-case-study-caa01a61dd86
 # Requirements
 # 1. Print Expenses
-# 2. Find SPECIFIC Expense 
-# 3. Calculate Expenses 
+# 2. Find SPECIFIC Expense
+# 3. Calculate Expenses
 
-'''
+"""
 Functions:
 
 1- Separating functions and data - ✓
 2- Assigning a function to a variable - ✓
 3- Create a list of functions and use that list ✓
 4- Passing functions as arguments ✓
-5- Returning functions ✓
-6- Mapping ✓
+5- Returning functions 
+6- Mapping 
 7- Filtering ✓
-8- Reducing ✓
+8- Reducing 
 9- Lambdas ✓
 10- List Comprehensions ✓
 11- Recursion ✓
 
-'''
+"""
 ############################################################################################
 expenses = None
 
+
+def validate_int(msg):
+    while True:
+        try:
+            intInput = int(input(msg))
+
+        except ValueError:
+            print("\nInvalid Input!\n")
+            continue
+        
+        else:
+            return intInput
+
+
 def read_data():
-    global expenses 
+    global expenses
     file_name = input("Enter File Name: ")
-    
+
     try:
         f = open(file_name,)
         expenses = json.load(f)
-        
+
     except ValueError:
         print("\nInvalid File Format!")
         exit()
@@ -43,59 +58,79 @@ def read_data():
     except FileNotFoundError:
         print("\nFile does not exist!")
         exit()
-        
+
     f.close()
 
-def sortbyDate():
+
+def sort_by_date():
     try:
-        return sorted(expenses, key=lambda x: datetime.strptime(x['date'],"%m/%d/%Y"), reverse=True)
+        return sorted(expenses,key=lambda x: datetime.strptime(x["date"], "%m/%d/%Y"),reverse=True)
     except ValueError:
         print("Invalid Date Format! Make sure all your data is valid (MM/DD/YYYY).")
 
+
 def print_expenses(length):
-    sorted_exp = sortbyDate()      
-    print(json.dumps(sorted_exp[:length], indent=3, ensure_ascii=False))
-    
-def find_expenses():
-    #7,10,6
-    print("")
+    print(*['{}'.format(iter) for iter in sort_by_date()[:length]], sep='\n')
 
-def calc_expenses():
-    #2,3,4,5,8
-    print("")
-
-def print_menu():
-    #11
-    print("1. Print Expenses\n2. Find Transaction\n3. Calculate Expenses\n4. Exit")
-    menu_in = int(input("Select Option: "))
-
-    if(menu_in == 1):
-        lim = -1
-        while not (lim>=0 and lim<=len(expenses)):
-            lim = int(input("\nNumber of Expenses to print (1-1000): "))
-            
-        print_expenses(lim)
-        
-    elif(menu_in == 2):            
-        find_expenses()
-
-    elif(menu_in == 3): 
-        calc_expenses()
-
-    elif(menu_in == 4):
+def return_by_id(val):
+    try:
+        return list(filter(lambda x: x["id"] == int(val), sort_by_date()))
+    except ValueError:
+        print("Value is not an Integer!")
         exit()
 
-    else: 
+def return_by_reference(val):
+    return list(filter(lambda x: x["transaction_reference"].lower() == val, sort_by_date()))
+
+def return_by_receiver_name(val):
+    return list(filter(lambda x: x["receiver_name"].lower() == val, sort_by_date()))
+
+def return_by_expense_type(val):
+    return list(filter(lambda x: x["expense_type"].lower() == val, sort_by_date()))
+
+def find_expenses(ch, val):
+    categories = [return_by_id,return_by_reference,return_by_receiver_name,return_by_expense_type]
+    print(*['{}'.format(iter) for iter in categories[ch](val.lower())], sep='\n')
+
+
+def calc_expenses():
+    
+    print("")
+
+
+def print_menu():
+    #1, 2, 11
+    GetInt = validate_int
+    print("1. Print Expenses\n2. Find Transaction\n3. Calculate Expenses\n4. Exit")
+    menu_in = GetInt("Select Option: ")
+
+    if menu_in == 1:
+        lim = -1
+        while not (lim >= 0 and lim <= len(expenses)):
+            lim = GetInt("Number of Expenses to print (1-1000): ")
+        print_expenses(lim)
+
+    elif menu_in == 2:
+        ch = -1
+        while not (ch >= 0 and ch <= 4):
+            ch = GetInt("\n1. Transaction ID\n2. Transaction Reference\n3. Receiver Name\n4. Expense Type\n\nChoose a Category:")
+        find_expenses(ch-1, input("\nEnter a value to search for: "))
+
+    elif menu_in == 3:
+        calc_expenses()
+
+    elif menu_in == 4:
+        exit()
+
+    else:
         print("\nInvalid Selection!\n")
         print_menu()
 
 
 def main():
-    flag = True
     read_data()
     print_menu()
-                
 
-if __name__ == '__main__':
-    main()    
 
+if __name__ == "__main__":
+    main()
